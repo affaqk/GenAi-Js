b9e4f48999313907c4673f51a74eac4ae9bc4f78
= <=>
2026-06-23 23:07:07 +0500

    settimeout and settimeinterval

--- dom.js
+++ dom.js
@@ -31,8 +31,43 @@
 //     heading.innerText = "I am green"
 // })
 
+// const body = document.querySelector("body");
+// const btn = document.querySelector(".change-color-btn")
+
+// const changeBackgroundColor = () => {
+//     let red = Math.floor(Math.random() * 255);
+//     let green = Math.floor(Math.random()*255)
+//     let blue = Math.floor(Math.random()*255);
+//     console.log(red,green,blue)
+//     /* rgb(55,88,109) */
+//     let color = `rgb(${red},${green},${blue})`
+//     // console.log(color)
+//     body.style.backgroundColor = color
+// }
+
+
+// btn.addEventListener("click", ()=>{
+//     changeBackgroundColor()
+// })
+
+// 0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f
+
+// asynchronous
+// synchronous
+
+// console.log("Line 1")
+// setTimeout(()=>{
+//     console.log("Line 2")
+// },5000)
+// for(let i = 0; i <= 50000; i++){
+//     console.log("Line 2.5")
+// }
+// console.log("Line 3")
+
+// event loop
+
+
 const body = document.querySelector("body");
-const btn = document.querySelector(".change-color-btn")
 
 const changeBackgroundColor = () => {
     let red = Math.floor(Math.random() * 255);
@@ -45,6 +80,13 @@ const changeBackgroundColor = () => {
     body.style.backgroundColor = color
 }
 
-btn.addEventListener("click", ()=>{
+// setTimeout(()=>{
+//     changeBackgroundColor()
+// },1000)
+
+let id = setInterval(()=>{
     changeBackgroundColor()
-})
+},1000);
+
+console.log(id)
+clearInterval(id)
